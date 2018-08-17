c59d058fbf8e576ca5dbb2a88345a03685d9793d
matthew-conrad <matthew.conrad@outlook.com>
2018-08-17 11:14:53 -0700

    Adjust normalize and _containers.scss

--- public/views/pages/about.blade.php
+++ public/views/pages/about.blade.php
@@ -2,9 +2,12 @@
 @section('title', 'About')
 @section('content')
 <div id="about">
-  <p>
-    Search points of interest, attractions, media, and maps of different locations around the world.
-  </p>
-  <p>Copyright &copy; <span id="year"></span> Matthew Conrad.</p>
+  <h2>TravelStar <span id="font-reg">| Your Destination for Travel Information</span></h2>
+  <div id="about-sub">
+    <p>
+      Search points of interest, attractions, media, and maps of different locations around the world.
+    </p>
+    <p>Copyright &copy; <span id="year"></span> Matthew Conrad.</p>
+  </div>
 </div>
 @stop
