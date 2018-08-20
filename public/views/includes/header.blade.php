736ae675e5a631d07ae7b7b8e038a04aa427fdca
matthew-conrad <matthew.conrad@outlook.com>
2018-08-19 17:33:15 -0700

    Add mobile nav

--- public/views/includes/header.blade.php
+++ public/views/includes/header.blade.php
@@ -1,8 +1,27 @@
-<div id="main-logo-container">
-  <img id="main-logo" src="img/travelstar-logo.svg" alt="TravelStar">
+<!-- Header Mobile -->
+<nav class="nav-sm">
+  <button id="hamburger" class="hamburger hamburger--3dx">
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+  </button>
+  <img id="main-logo-sm" src="img/travelstar-logo.svg" alt="TravelStar Mobile">
+</nav>
+<div class="mobile-menu__nav">
+  <ul class="mobile-menu__dropdown">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="firm.html">Firm</a></li>
+    <li><a href="expertise.html">Expertise</a></li>
+    <li><a href="portfolio.html">Portfolio</a></li>
+    <li><a href="contact.html">Contact</a></li>
+  </ul>
+</div>
+<!-- Header Desktop -->
+<div class="nav-lg">
+  <img id="main-logo-lg" src="img/travelstar-logo.svg" alt="TravelStar Desktop">
   <h1 id="main-logo-txt">TravelStar</h1>
 </div>
-<ul id="menu-lg">
+<ul class="menu-lg">
   <li><a href='/'>Home</a></li>
   <li><a href='/about'>About</a></li>
 </ul>
