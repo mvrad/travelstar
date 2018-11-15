bf0589b4875b2b617d54eb8de378b271ba89df94
matthew-conrad <matthew.conrad@outlook.com>
2018-11-15 06:22:27 -0800

    Remove content security policy directives

--- config/secure-headers.php
+++ config/secure-headers.php
@@ -11,20 +11,8 @@ return [
 
     // Content Security Policy
     'csp' => [
-        'default' => [
-            'self',
-        ],
         'img-src' => [
             '*', // Allow images from anywhere
-        ],
-        'style-src' => [
-            'self',
-            'unsafe-inline', // Allow inline styles
-            'https://fonts.googleapis.com', // Allow stylesheets from Google Fonts
-        ],
-        'font-src' => [
-            'self',
-            'https://fonts.gstatic.com', // Allow fonts from the Google Fonts CDN
         ],
     ],
 ];
