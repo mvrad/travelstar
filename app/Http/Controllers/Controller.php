deae9cb6e4520d37b7fac2aa2196cfbc4452188d
matthew-conrad <matthew.conrad@outlook.com>
2018-12-02 15:07:54 -0800

    Configure Laravel encrypt helper

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,31 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+// Encrypter
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+
+class UserController extends Controller
+{
+    /**
+     * Store a secret message for the user.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function storeSecret(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->fill([
+            'secret' => encrypt($request->secret)
+        ])->save();
+    }
 }
